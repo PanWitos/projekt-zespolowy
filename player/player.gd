8e7f6e293d36a3b805e899ed1e9a248e37e999c6
Withos <withos3@gmail.com>
2022-04-11 21:42:21 +0200

    multiple jumps

--- player/player.gd
+++ player/player.gd
@@ -5,6 +5,7 @@ const MAX_SPEED_H = 650
 const MAX_SPEED_V = 1600
 const FRICTION = 80000
 const GRAVITY = 1300
+const JUMP_SPEED = -1600
 
 onready var animation_player = $AnimationPlayer
 onready var sprite = $Sprite
@@ -12,13 +13,15 @@ onready var sprite = $Sprite
 var velocity_h = 0
 var velocity_v = 0
 var velocity = Vector2.ZERO
-var JUMPS = 2
+var ADDITIONAL_JUMPS = 0
+var JUMPS_LEFT = 0
 var acceleration_v = 0
 
 func _ready():
 	animation_player.play("Idle")
 
 func numberMoveToward(from, to, delta):
+	
 	if from < to:
 		if from + delta <= to:
 			return from + delta
@@ -40,9 +43,16 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * MAX_SPEED_H
 	
-	if Input.is_action_just_pressed("ui_up") && JUMPS > 0:
-		velocity_v = -1600
-		acceleration_v = 0
+	if is_on_floor():
+		JUMPS_LEFT = ADDITIONAL_JUMPS
+		if Input.is_action_just_pressed("ui_up"):
+			velocity_v = JUMP_SPEED
+			acceleration_v = 0
+	else:
+		if Input.is_action_just_pressed("ui_up") && JUMPS_LEFT > 0:
+			velocity_v = JUMP_SPEED
+			acceleration_v = 0
+			JUMPS_LEFT -= 1
 		
 	input_vector.y = MAX_SPEED_V
 	
