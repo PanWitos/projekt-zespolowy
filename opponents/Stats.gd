712738f77c33a463266191493f78005dd1107af8
Amelia Kwiecinska <kwam0015@gmail.com>
2022-05-23 20:58:44 +0200

    squirrwiel mechnics

--- opponents/Stats.gd
+++ opponents/Stats.gd
@@ -1,4 +1,11 @@
 extends Node
-export(int) var max_health =1
-var health = max_health
 
+export(int) var max_health = 1
+onready var health = max_health setget set_health
+
+signal no_health
+
+func set_health(value):
+	health = value
+	if health <= 0:
+		emit_signal("no_health")
---
+++ player/SwordHitbox.gd
@@ -0,0 +1,3 @@
+extends "res://Hitboxes_and_Hurtboxes/hitbox.gd"
+
+var knockback_vector = Vector2.ZERO
