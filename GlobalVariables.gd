960eeeb8fa6bae815752833e6fae161297ebaff5
PanWitos <72377565+PanWitos@users.noreply.github.com>
2022-06-13 18:59:24 +0200

    Added save/load

--- GlobalVariables.gd
+++ GlobalVariables.gd
@@ -1,3 +1,28 @@
 extends Node
 
 var isGrassBossDead = false
+
+var lastSaveStation = null
+
+var list = []
+
+func _ready():
+	SignalBus.connect("worldList", self, "updateList")
+
+func save():
+	
+	var save_dict = {
+		"filename" : get_filename(),
+		"parent" : get_parent().get_path(),
+		"isGrassBossDead" : isGrassBossDead,
+		"lastSaveStation" : lastSaveStation,
+		"list" : list
+	}
+	print("GlobalVariables save")
+	return save_dict
+
+func updateList(name):
+	if name in list:
+		pass
+	else:
+		list.append(name)
