3ed1066560f50c6dc84a7d9a1139e98f276539d4
PanWitos <72377565+PanWitos@users.noreply.github.com>
2022-06-13 16:50:53 +0200

    Added bossfight mechanic

--- opponents/goblin_king/goblin_king.gd
+++ opponents/goblin_king/goblin_king.gd
@@ -3,12 +3,14 @@ extends Node
 onready var stats = $Stats
 var knockback = Vector2.ZERO
 
+
 func _on_hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockbackVector * 120
 
 
 func _on_Stats_no_health():
+	GlobalVariables.isGrassBossDead = true
 	queue_free()
 
 	
